a8f14496a598f1a188046a5c812b486cd8e69f4f
Diego <you@example.com>
2024-10-23 16:21:13 +0200

    Lens Fertig

---
+++ at/diego/Lens.java
@@ -0,0 +1,20 @@
+package at.diego;
+
+public class Lens {
+    private String manufacturer;
+    private float brennweite;
+
+    public Lens(String manufacturer, float brennweite) {
+        this.manufacturer = manufacturer;
+        this.brennweite = brennweite;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public float getFocalLength() {
+        return brennweite;
+    }
+
+}
