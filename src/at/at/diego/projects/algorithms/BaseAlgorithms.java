d11d3bb132fd7f81b2faffb4eea45fcd7f158da0
Diego <you@example.com>
2024-12-19 12:06:05 +0100

    BaseAlgorithms Fertig

--- src/at/at/diego/projects/algorithms/BaseAlgorithms.java
+++ src/at/at/diego/projects/algorithms/BaseAlgorithms.java
@@ -8,4 +8,24 @@ public class BaseAlgorithms {
         }
         return dataReversed;
     }
+
+    public static int max(int[] data) {
+        int max = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] data) {
+        int min = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+        return min;
+    }
 }
