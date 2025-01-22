db5d626d8f0309f79b6c53bec5e52b428e25a9b9
Diego <you@example.com>
2025-01-22 13:36:36 +0100

    DataGenerator  max + 1 so das Höchste Grenze incl ist

--- src/at/at/diego/projects/algorithms/DataGenerator.java
+++ src/at/at/diego/projects/algorithms/DataGenerator.java
@@ -4,26 +4,26 @@ import java.util.Random;
 
 public class DataGenerator {
     public static int[] generateDataArray(int size) {
+        Random rand = new Random();
         int[] data = new int[size];
-        Random random = new Random();
         for (int i = 0; i < size; i++) {
-            data[i] = random.nextInt();
+            data[i] = rand.nextInt();
         }
         return data;
     }
 
-    public static int[] generateDataArrayWithMinMax(int size, int min, int max) {
+    public static int[] generateDataArray2(int size, int min, int max) {
+        Random rand = new Random();
         int[] data = new int[size];
-        Random random = new Random();
         for (int i = 0; i < size; i++) {
-            data[i] = random.nextInt(max + 1 - min) + min;
+            data[i] = rand.nextInt(max + 1 - min) + min;
         }
         return data;
     }
 
-    void printArray(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+    public static void printArray(int[] data) {
+        for (int num : data) {
+            System.out.print(num + " ");
         }
     }
 }
